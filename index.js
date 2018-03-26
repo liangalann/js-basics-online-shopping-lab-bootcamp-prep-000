e7714dabef4a96f932e5957a5da61432c3ec4e87
Alan Liang <liangalann@gmail.com>
2018-03-26 00:17:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,32 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var price = Math.floor(Math.random()*100)
+ cart.push(new Object({[item]:price}))
+ return cart
 }
-
-function viewCart() {
+function viewCart(item) {
   // write your code here
+
+  if (cart.length === 0) {
+    var empty = ("Your shopping cart is empty.")
+    return empty
+  } else {
+    var sentence = ("In the cart you have")
+    for (var i = 0; i < cart.length; i++) {
+    for (var item in cart[i]) {
+      sentence += "{itemName} at {cart[i][itemPrice]}"
+      if(i!==cart.length-1){
+            sentence+=","
+      }
+          else {
+            sentence+= "."
+          }
+    }
+      return sentence
+    }
+  }
+
 }
 
 function total() {
@@ -28,3 +50,5 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
 }
+
+console.log(cart)
